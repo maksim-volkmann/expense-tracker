c438bf7795ca751495c3361d2a90531d6769fed3
Maksim <auksinistonic@gmail.com>
2022-09-15 14:57:30 +0300

    feat: split code to more managable blocks

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
+import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 9, 14)
-  const expenseTitle = 'Car Insurance'
-  const expenseAmout = '965.65'
-
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmout}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 
